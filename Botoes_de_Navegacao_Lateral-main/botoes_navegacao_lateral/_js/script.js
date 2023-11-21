e55065cd3713e03296df148fcee3e12ad9a337cb
lelugoamx <128144002+lelugoamx@users.noreply.github.com>
2023-11-20 21:02:19 -0300

    Add files via upload

--- Botoes_de_Navegacao_Lateral-main/botoes_navegacao_lateral/_js/script.js
+++ Botoes_de_Navegacao_Lateral-main/botoes_navegacao_lateral/_js/script.js
@@ -1,38 +1,54 @@
 
-document.addEventListener("DOMContentLoaded", inicializar);
+document.addEventListener("DOMContentLoaded", iniciar);
 
-function inicializar() {
-  let btnDropdown = document.getElementById("btnDropdown");
-  btnDropdown.addEventListener("click", alternarDropdown);
+  function iniciar() {
+    let btnDropdowndois = document.getElementById("btnDropdowndois");
+    btnDropdowndois.addEventListener("click", alternarDropdowndois);
 
+    document.addEventListener("click", fecharDropdown);
+  }
 
-  document.addEventListener("click", fecharDropdown);
-}
+  function alternarDropdowndois(event) {
+    let menuDropdown  = document.getElementById("divMenuDropdowndois");
+    menuDropdown.classList.toggle("exibirdois");
+    event.stopPropagation();
+  }
 
-
-function alternarDropdown(event) {
-  
-  let menuDropdown  = document.getElementById("divMenuDropdown");
-
-  menuDropdown.classList.toggle("exibir");
-  event.stopPropagation();
-
-
-function fecharDropdown(event) {
- 
-  if (!event.target.matches('.estiloBtnDropdown')) {
-   
-    let menusDropdown = document.getElementsByClassName("conteudoDropdown");
-    
-
-    for (let i = 0; i < menusDropdown.length; i++) {
-      let menuAberto = menusDropdown[i];
-      
-
-      if (menuAberto.classList.contains('exibir')) {
-        menuAberto.classList.remove('exibir');
+  function fecharDropdown(event) {
+    if (!event.target.matches('.estiloBtnDropdowndois')) {
+      let menusDropdown = document.getElementsByClassName("conteudoDropdowndois");
+      for (let i = 0; i < menusDropdown.length; i++) {
+        let menuAberto = menusDropdown[i];
+        if (menuAberto.classList.contains('exibirdois')) {
+          menuAberto.classList.remove('exibirdois');
+        }
       }
     }
   }
-}
-}
+
+  document.addEventListener("DOMContentLoaded", inicializar);
+
+  function inicializar() {
+    let btnDropdown = document.getElementById("btnDropdown");
+    btnDropdown.addEventListener("click", alternarDropdown);
+
+    document.addEventListener("click", fecharDropdown);
+  }
+
+  function alternarDropdown(event) {
+    let menuDropdown  = document.getElementById("divMenuDropdown");
+    menuDropdown.classList.toggle("exibir");
+    event.stopPropagation();
+  }
+
+  function fecharDropdown(event) {
+    if (!event.target.matches('.estiloBtnDropdown')) {
+      let menusDropdown = document.getElementsByClassName("conteudoDropdown");
+      for (let i = 0; i < menusDropdown.length; i++) {
+        let menuAberto = menusDropdown[i];
+        if (menuAberto.classList.contains('exibir')) {
+          menuAberto.classList.remove('exibir');
+        }
+      }
+    }
+  }
